79adcce7b155f2b4434b5a8ee0119421f5dcdbd9
seervipradhyumn04-ship-it <seervipradhyumn04@gmail.com>
2026-06-13 21:38:33 +0530

    Create question1.py

---
+++ question1.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, age, course):
+        self.name = name
+        self.age = age
+        self.course = course
+
+s1 = Student("Pradhyumn", 20, "Data Science")
+
+print("Name:", s1.name)
+print("Age:", s1.age)
+print("Course:", s1.course)
